3cba4c555b6d518fb7e97f66dac5ba6e009f9314
twtzptr <wildhare2020@yandex.ru>
2022-09-12 12:57:25 +0300

    cors

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.use(cookieParser());
+  app.enableCors();
 
   const swaggerConfig = new DocumentBuilder()
     .setTitle('Outside task')
